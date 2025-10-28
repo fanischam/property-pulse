aa5194482ebde65023d0de0e71ed657edb5c722d
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-10-28 15:05:30 +0200

    Created property pages with dynamic title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ type RootLayoutProps = {
 export const metadata: Metadata = {
   title: {
     default: 'Property Pulse',
-    template: '%s | My App',
+    template: '%s | Property Pulse',
   },
   keywords: 'rental, property, real estate',
   description: 'Find the perfect rental property',
---
+++ app/properties/[...id]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+
+// change page title
+export const metadata: Metadata = {
+  title: 'Property',
+};
+
+const PropertyPage = () => {
+  return <div>Property Page</div>;
+};
+
+export default PropertyPage;
