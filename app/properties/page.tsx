25b9ce4a2a35ae833b62fca8257a84f5a2879389
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-10-28 20:33:22 +0200

    created reusable property card component to display properties and included it inside the properties page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,11 +1,28 @@
 import { Metadata } from 'next';
+import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
 
 export const metadata: Metadata = {
   title: 'Properties',
 };
 
 const PropertiesPage = () => {
-  return <div>Properties Page</div>;
+  console.log(properties);
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length == 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+            {properties.map((prop) => (
+              <PropertyCard property={prop} key={prop._id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
