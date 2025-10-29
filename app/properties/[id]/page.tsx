782cd65815cb0e62a9595f2e416162cd8fda4790
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-10-29 18:40:08 +0200

    Introduced property card component including property details and added it inside the property page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PropertyDetails from '@/components/PropertyDetails';
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import connectDb from '@/config/database';
 import Property from '@/models/Property';
@@ -42,7 +43,9 @@ const PropertyPage = async ({ params }: PropertyPageProps) => {
       </section>
       <section className='bg-blue-50'>
         <div className='container m-auto py-10 px-6'>
-          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'></div>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            <PropertyDetails property={property} />
+          </div>
         </div>
       </section>
     </>
