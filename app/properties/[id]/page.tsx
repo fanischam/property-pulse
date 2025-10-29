c1f47056e4bb77e1eeee07eeb44051f9e360f3e0
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-10-29 18:16:17 +0200

    Created Propert header image and added some elements in property page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,10 @@
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import connectDb from '@/config/database';
+import Property from '@/models/Property';
+import { IProperty } from '@/types/types';
 import { Metadata } from 'next';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
 // change page title
 export const metadata: Metadata = {
@@ -13,7 +19,34 @@ type PropertyPageProps = {
 
 const PropertyPage = async ({ params }: PropertyPageProps) => {
   const { id } = await params;
-  return <div>Property Page {id}</div>;
+  await connectDb();
+
+  const property = await Property.findById(id).lean<IProperty>();
+
+  if (!property) {
+    return <section>No property found</section>;
+  }
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className='bg-blue-50'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'></div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
