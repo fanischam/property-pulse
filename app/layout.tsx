fac14c445d689ed14589c0b958542401dedded56
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-11-06 20:43:52 +0200

    Added auth provider in layout file, using cookes for getting the session and storing a dummy user

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { ReactNode } from 'react';
 import Navbar from '@/components/Navbar';
 import '@/assets/styles/globals.css';
 import Footer from '@/components/Footer';
+import { cookies } from 'next/headers';
+import AuthProvider from './providers/AuthProvider';
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -17,13 +19,24 @@ export const metadata: Metadata = {
   description: 'Find the perfect rental property',
 };
 
-const RootLayout = ({ children }: RootLayoutProps) => {
+const RootLayout = async ({ children }: RootLayoutProps) => {
+  const session = (await cookies()).get('session')?.value;
+
+  const initialAuth = session
+    ? {
+        isLoggedIn: true,
+        user: { id: 'user_id', name: 'User Name', email: 'user@example.com' },
+      }
+    : { isLoggedIn: false, user: null };
+
   return (
     <html>
       <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+        <AuthProvider initialAuth={initialAuth}>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
