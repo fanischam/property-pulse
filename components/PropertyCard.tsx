156472fd719f18f1c1ac323ee1b795f0c8974fae
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-11-09 15:46:47 +0200

    removed consumption of local images

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -24,7 +24,7 @@ const PropertyCard = ({ property }: PropertyCardProps) => {
   return (
     <div className='bg-white rounded-xl shadow-md relative'>
       <Image
-        src={`/images/properties/${property.images[0]}`}
+        src={property.images[0]}
         alt=''
         width='0'
         height='0'
