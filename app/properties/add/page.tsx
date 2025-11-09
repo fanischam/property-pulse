c48c0a018f178dcba08f5d3f5886e2cff0ed5ead
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-11-09 13:57:20 +0200

    Created form to add new property

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,4 +1,5 @@
 import { verifyJwt } from '@/app/lib/jwt';
+import AddPropertyForm from '@/components/AddPropertyForm';
 import { Metadata } from 'next';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
@@ -16,7 +17,15 @@ const AddPropertyPage = async () => {
     redirect('/login?redirect=/properties/add');
   }
 
-  return <div>Add Property</div>;
+  return (
+    <section className='bg-blue-50'>
+      <div className='container m-auto max-w-2xl py-24'>
+        <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
+          <AddPropertyForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddPropertyPage;
