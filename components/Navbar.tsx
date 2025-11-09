4e5b406ccca7b533988d596bfda780369bfddab4
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-11-09 13:18:31 +0200

    enhanced login or register button styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -109,7 +109,7 @@ const Navbar = () => {
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
                 <Link href='/login'>
-                  <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                  <button className='flex items-center text-white bg-gray-700 cursor-pointer hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
                     <span>Login or Register</span>
                   </button>
                 </Link>
