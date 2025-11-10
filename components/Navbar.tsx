8e2dedcf4393881a0e767649783e70bd708ee703
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-11-10 20:53:03 +0200

    added code to close profile dropdown on option click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -184,6 +184,7 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex={-1}
                       id='user-menu-item-0'
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Your Profile
                     </Link>
@@ -193,6 +194,7 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex={-1}
                       id='user-menu-item-2'
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Saved Properties
                     </Link>
