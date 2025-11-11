89df61df50a9d08c5af49f258bbd4ebe90554814
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-11-11 21:52:59 +0200

    replaced hardcoded 70%/30% layout with custom tailwind utility class

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -57,7 +57,7 @@ const PropertyPage = async ({ params }: RouteParams) => {
       </section>
       <section className='bg-blue-50'>
         <div className='container m-auto py-10 px-6'>
-          <div className='grid grid-cols-1 md:grid-cols-[70%_30%] w-full gap-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70-30 w-full gap-6'>
             <PropertyDetails property={property} />
             <aside className='space-y-4'>
               <BookmarkButton property={property} />
